3e217352530293a186f0b57f9a22db84f3add0b2
Guilherme Notebook <guillhermeym2009@gmail.com>
2024-12-08 06:57:03 -0300

    Ainda tentnado sincronizar o meu pc com o meu notebook

--- src/Components/MainComponents.jsx
+++ src/Components/MainComponents.jsx
@@ -2,6 +2,14 @@
 
 import useLocal from "@/Hook/useLocal";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@shadcn/ui";
 
 export default function MainComponents() {
   const [showMessage, setShowMessage] = useState(false);
@@ -20,12 +28,47 @@ export default function MainComponents() {
 
   return (
     <>
-      <main className="bg-backgroundMain flex-col gap-48 w-full h-mainHeight">
-        <aside>Asided</aside>
-        <article>
-            article
-            <section></section>
+      <main className="bg-backgroundMain flex-col gap-48 w-full h-mainHeight py-3">
+        {/*Este aside será o campo de pesquisa */}
+        <aside className="flex gap-2 bg-backgroundAside w-72 items-center py-3 px-4 mx-auto rounded-md">
+          ícone
+          <div className="text-colorTextAside text-sm">
+            Digite para procurar alguma nota
+          </div>
+        </aside>
+        <article className="h-articleHeight w-full">
+          {showMessage ? (
+            <section className="mx-auto text-colorText flex items-center justify-center">
+              <p>
+                Você ainda não criou nenhuma anotação, por que não tenta criar
+                um? <Link href={"/"}>Clique aqui</Link>
+              </p>
+            </section>
+          ) : (
+            <section>
+              <p>Com notas</p>
+            </section>
+          )}
+          <section></section>
         </article>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="px-4 py-2 bg-blue-500 text-white rounded-md">
+              Ações
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-white rounded-md shadow-md">
+            <DropdownMenuItem onSelect={() => alert("Primeira ação")}>
+              Primeira ação
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => alert("Segunda ação")}>
+              Segunda ação
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => alert("Terceira ação")}>
+              Terceira ação
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        backgroundHeader: "var(--background-header)",
-        colorText: "var(--color-text)",
-        backgroundButton: "var(--background-button)",
-        colorLink: "var(--color-link)",
-        backgroundMain: "var(--background-main)",
-      },
-      height: { 
-        'mainHeight': 'calc(100vh - 80px)',
-      }
-    },
+  	extend: {
+  		colors: {
+  			backgroundHeader: 'var(--background-header)',
+  			colorText: 'var(--color-text)',
+  			backgroundButton: 'var(--background-button)',
+  			colorLink: 'var(--color-link)',
+  			backgroundMain: 'var(--background-main)',
+  			colorTextAside: 'var(--colorText-aside)',
+  			backgroundAside: 'var(--background-aside)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		height: {
+  			mainHeight: 'calc(100vh - 80px)',
+  			articleHeight: 'calc(100vh - 137)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
